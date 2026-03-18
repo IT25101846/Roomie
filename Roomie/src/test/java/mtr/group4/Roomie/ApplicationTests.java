eeb44afd78bde0c70a07a1cf23d7b5e270b4bd72
Tharaka Deshan <it25101846@my.sliit.lk>
2026-03-18 09:33:19 +0530

    Initial project setup

---
+++ Roomie/src/test/java/mtr/group4/Roomie/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mtr.group4.Roomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

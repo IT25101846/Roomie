67576b0653813186b01dbeb308c303bc32bd2256
it25101494 <it25101494@my.sliit.lk>
2026-04-13 00:13:11 +0530

    restore

--- Roomie/src/main/java/mtr/group4/Roomie/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package mtr.group4.Roomie.exception;
-
-public class DuplicateEmailException extends RoomieException {
-    public DuplicateEmailException(String email) {
-        super("An account with email '" + email + "' already exists.");
-    }
-}
--- Roomie/src/main/java/mtr/group4/Roomie/exception/RoomieException.java
+++
@@ -1,7 +0,0 @@
-package mtr.group4.Roomie.exception;
-
-public class RoomieException extends Exception {
-    public RoomieException(String message) {
-        super(message);
-    }
-}
